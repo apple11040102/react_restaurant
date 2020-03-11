da5edf60b7b292645cb3aef3ff6c277ee71c0f1c
dongdong <dong1111@gmail.com>
2020-03-11 13:58:12 +0800

    commit_project

---
+++ build/precache-manifest.036cbd7c39203187f3a82c8cab4aa660.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "08494823974d9eb11953",
+    "url": "/static/css/main.d09881e2.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "59fb499eef44e4f9daaa",
+    "url": "/static/js/2.41e000f5.chunk.js"
+  },
+  {
+    "revision": "ae7bcf5b802b73c7c73dbd7b0db0272e",
+    "url": "/static/media/logo-gray.ae7bcf5b.jpg"
+  },
+  {
+    "revision": "08494823974d9eb11953",
+    "url": "/static/js/main.2d5f1459.chunk.js"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "59fb499eef44e4f9daaa",
+    "url": "/static/css/2.48d6118c.chunk.css"
+  },
+  {
+    "revision": "305ca3fa25b8ef305e4664f986a9469f",
+    "url": "/index.html"
+  }
+];
